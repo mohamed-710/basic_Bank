c582ceb5570e09d67585ab8a3f7b6445e78df5a4
mohamed-710 <moha2023toedu@gmail.com>
2025-03-11 15:43:27 +0200

    8th

--- config/DB.js
+++ config/DB.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI, {
-
-        });
-
+        const db = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${db.connection.host}`);
         console.log("MongoDB Connected");
     } catch (err) {
         console.error("Error");
--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.use(express.json());
 app.use(cookieParser()); 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true, 
+  origin: "*", 
+  credentials: false, 
 }));
 
 app.use("/api/auth", authRoutes);
